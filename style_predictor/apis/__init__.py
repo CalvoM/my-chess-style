e6a442d0cdedbf7a763ccb87ae97c54a94794390
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-04-18 10:34:19 +0300

    docs: Update the openapi description.

--- style_predictor/apis/__init__.py
+++ style_predictor/apis/__init__.py
@@ -2,10 +2,15 @@ from ninja import NinjaAPI
 from style_predictor.apis.pgn.api import router as pgn_router
 from style_predictor.apis.analysis.api import router as analysis_router
 
-api = NinjaAPI(
-    title="My Chess Style API",
-    description="API to interact with our my chess style model",
-)
+api_description = """
+Welcome to the *My Chess Style API*.
+
+`My Chess Style` is a machine learning model that seeks to determine a player's
+playing style. <br>
+It is an ambitious project since I have no knowledge of ML, so praying for me and wish me luck.
+"""
+
+api = NinjaAPI(title="My Chess Style API", description=api_description)
 
 
 api.add_router("/pgn/", pgn_router)
