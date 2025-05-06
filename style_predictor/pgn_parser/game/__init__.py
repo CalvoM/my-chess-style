f1b9ceca25539bd420c0add409f9b47b0d0a01ef
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-05-06 13:53:03 +0300

    tasks: implement multistep analysis
    The user can get gradual update of the analysis.

--- style_predictor/pgn_parser/game/__init__.py
+++ style_predictor/pgn_parser/game/__init__.py
@@ -1,12 +1,12 @@
 __all__ = ("PGNGame",)
 
-from style_predictor.pgn_parser.file_processing.lexer import Lexer, Token
-from style_predictor.pgn_parser.file_processing.parser import Parser
-
 from .game import PGNGame
 
 
 def get_games(data: str) -> list[PGNGame] | None:
+    from style_predictor.pgn_parser.file_processing.lexer import Lexer, Token
+    from style_predictor.pgn_parser.file_processing.parser import Parser
+
     tokens: list[Token] | None = Lexer(data).lex()
     games: list[PGNGame] | None = None
     if tokens:
