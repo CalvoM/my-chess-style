6741ba47669cf99a0b8a55e4456e54dcd0a81c34
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-07-10 15:30:43 +0300

    feat(frontend): Init commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Button from '@/volt/Button.vue'
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="bg-blue-500 text-white p-5"">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+    <Button label="Test" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
