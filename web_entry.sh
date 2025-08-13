001fcf16f5d2d851693370ba90d6381572e50c3a
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-08-13 17:12:00 +0300

    feat(docker): We can now serve the frontend from docker image.

--- web_entry.sh
+++ web_entry.sh
@@ -6,4 +6,5 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py import_chess_openings
 
-exec python manage.py runserver 0.0.0.0:8000
+# exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn my_chess_style.wsgi -b 0.0.0.0:8000
